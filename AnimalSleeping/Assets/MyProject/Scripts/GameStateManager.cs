dc2dbd6766314a29622f5d3e4bde51e2740dd586
TomohikoToyama <golden.firework.launcher@gmail.com>
2018-11-19 23:53:24 +0900

    フォルダ構成修正

---
+++ AnimalSleeping/Assets/MyProject/Scripts/GameStateManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace SleepingAnimal
+{
+    public class GameStateManager : MonoBehaviour
+    {
+        enum SCENE
+        {
+            TITLE = 0,
+            MENU  = 1,
+            ROOM  = 2
+        }
+        private int currentScene;
+        private void Awake()
+        {
+
+            GameObject[] obj = GameObject.FindGameObjectsWithTag("GameStateManager");
+            if (obj.Length > 1)
+            {
+                // 既に存在しているなら削除
+                Destroy(gameObject);
+            }
+            else
+            {
+                // 音管理はシーン遷移では破棄させない
+                DontDestroyOnLoad(gameObject);
+            }
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+            currentScene = 0;
+            SoundManager.Instance.PlayBGM(0);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
+            {
+                
+                    SceneManager.LoadScene("Menu");
+                
+            }
+        }
+    }
+}
---
+++ AnimalSleeping/Assets/MyProject/Scripts/UserController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserController : MonoBehaviour {
+
+    // Use this for initialization
+
+    private void Awake()
+    {
+
+        GameObject[] obj = GameObject.FindGameObjectsWithTag("Player");
+        if (obj.Length > 1)
+        {
+            // 既に存在しているなら削除
+            Destroy(gameObject);
+        }
+        else
+        {
+            // 音管理はシーン遷移では破棄させない
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
