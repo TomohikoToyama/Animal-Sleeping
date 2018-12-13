e8618d75cec674e65c42b7618219af592fbd2daa
TomohikoToyama <golden.firework.launcher@gmail.com>
2018-12-13 23:59:52 +0900

    ルーム、動物のデータクラス追加

---
+++ AnimalSleeping/Assets/MyProject/Scripts/AnimalData.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalData : MonoBehaviour {
+
+    private int state;
+    public int State
+    {
+        get { return state; }
+        set { state = value; }
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
