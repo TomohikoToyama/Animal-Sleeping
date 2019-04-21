b3cd3451731b011d231a9c2622f08229e7f0e78c
TomohikoToyama <golden.firework.launcher@gmail.com>
2019-04-21 15:43:12 +0900

    ゲームモードの設定追加

--- AnimalSleeping/Assets/MyProject/Scripts/Game/PlayGameManager.cs
+++ AnimalSleeping/Assets/MyProject/Scripts/Game/PlayGameManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GamePlayManager : MonoBehaviour {
+public class PlayGameManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
